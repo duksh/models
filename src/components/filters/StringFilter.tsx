74897af6eb49b266daf55d1e8a66cf844d461879
Astrid Gealer <astrid@gealer.email>
2026-03-03 16:08:43 +0000

    fix string filters

--- src/components/filters/StringFilter.tsx
+++ src/components/filters/StringFilter.tsx
@@ -10,14 +10,28 @@ export default function StringFilter({
     query: ColumnQuery;
     updateQuery: (rerunQuery: boolean) => void;
 }) {
-    const [filterValue, setFilterValue] = React.useState<string>(
+    const filterValue = React.useRef<string>(
         query.columnFilters[columnName] || ""
     );
+
+    React.useEffect(() => {
+        filterValue.current = query.columnFilters[columnName] || "";
+    }, [query.columnFilters[columnName]]);
+
+    const setFilterValue = React.useCallback((val: string) => {
+        filterValue.current = val;
+        if (val === "") {
+            delete query.columnFilters[columnName];
+        } else {
+            query.columnFilters[columnName] = val;
+        }
+        updateQuery(false);
+    }, [columnName, query, updateQuery]);
 
     return (
         <input
             type="text"
-            value={filterValue}
+            value={filterValue.current}
             onChange={(e) => {
                 const val = e.target.value;
                 if (val === "") {
@@ -26,7 +40,6 @@ export default function StringFilter({
                     query.columnFilters[columnName] = val;
                 }
                 setFilterValue(val);
-                updateQuery(false);
             }}
             className="w-full border text-sm border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 rounded-md p-1 placeholder:text-gray-500 dark:placeholder:text-gray-400 placeholder:font-light"
             aria-label={`Filter ${columnName}`}
